76ced6e8998f2da67fcd3d8e4ed2d8a60c67d756
EV3KevinDEV <kevinholding92@gmail.com>
2024-12-05 19:02:40 -0800

    wdsa

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -80,9 +81,14 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {}
 
+
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {}
+  public static forward = (0.0);
+ 
+  double var = XboxController.getLeftX();
+
 
   /** This function is called once when the robot is disabled. */
   @Override
